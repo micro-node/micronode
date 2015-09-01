dc0349b1aeb644cea960eb44bfdc2f629728a31b
Haithem Bel Haj <haythem.belhaj@gmail.com>
2015-09-01 11:46:28 +0200

    support async services only

--- test/services/fibonacci/index.js
+++ test/services/fibonacci/index.js
@@ -1,12 +1,12 @@
 
 module.exports = fibonacci;
 
-function fibonacci(n){
+function fibonacci(n, callback){
 
   // avoid stack overflow with tail recursion
   return function fib(n, a, b) {
 
-    return n > 0 ? fib(n-1, b, a + b) : a;
+    return n > 0 ? fib(n-1, b, a + b) : callback(null, a);
   }(n,0,1);
 }
 
